b900648964f5d1d9d24ecdc6c505aab38cda7019
GuilhermeOOF <93491333+GuilhermeOOF@users.noreply.github.com>
2023-07-05 17:26:11 -0300

    Added "Remove" onclick

--- src/Js/app.js
+++ src/Js/app.js
@@ -1,12 +1,29 @@
 function SetImg(url){
-    const img = document.getElementById("qrCodeImg")
+    const img = document.getElementById("qrCodeImg");
+    
     img.setAttribute("src", url);
-}
+};
+
+function RomoveImg(){
+    const img = document.getElementById("qrCodeImg");
+    
+    img.setAttribute("src", null);
+};
 
 document.getElementById('Create').onclick = function() {
     let size = document.getElementById("NumberBox").value;
     let text = document.getElementById("TextBox").value;
 
-    let baseurl = "http://api.qrserver.com/v1/create-qr-code/?data="+text+"&size="+size+"x"+size
-    SetImg(baseurl)
+    const imgdiv = document.getElementById("ImgDiv");
+    imgdiv.classList.add("ImageShowing");
+    imgdiv.classList.remove("ImageHidden");
+    let baseurl = "http://api.qrserver.com/v1/create-qr-code/?data="+text+"&size="+size+"x"+size;
+    SetImg(baseurl);
+};
+
+document.getElementById('Remove').onclick = function() {
+    const imgdiv = document.getElementById("ImgDiv");
+    imgdiv.classList.remove("ImageShowing");
+    imgdiv.classList.add("ImageHidden")
+    RomoveImg();
 };
